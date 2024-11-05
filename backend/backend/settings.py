7f7b71763182127142c0639a7d3b10379dda96e6
Pirat102 <patryk.pirowski@gmail.com>
2024-11-05 10:53:50 +0100

    migrate to postgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASSWORD"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
+
 
 
 # Password validation
