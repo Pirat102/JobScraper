e15f98574736ea5f4b10c6bd0e2eed0260a97a7d
Pirat102 <patryk.pirowski@gmail.com>
2024-11-07 13:23:21 +0100

    frontend setup

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { ACCESS_TOKEN } from "./constants"
+
+// importing everything from env variable file
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+// Adding authorization header
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
+
+export default api
