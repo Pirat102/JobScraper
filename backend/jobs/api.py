db9988bed9aae1c9feb21e052ead29929fef3048
Pirat102 <patryk.pirowski@gmail.com>
2024-11-08 20:24:23 +0100

    improved registration

--- backend/jobs/api.py
+++ backend/jobs/api.py
@@ -1,9 +1,9 @@
-from ninja import Query, Form
+from ninja import Query
+from typing import Dict
 from jobs.models import Job
 from jobs.schemas import *
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-
+from ninja_extra.permissions import AllowAny
 from ninja_jwt.controller import NinjaJWTDefaultController
 from ninja_extra import NinjaExtraAPI, api_controller, route
 from ninja_jwt.authentication import JWTAuth
@@ -11,16 +11,19 @@ from ninja_jwt.authentication import JWTAuth
 api = NinjaExtraAPI(csrf=True)
 
 
-@api.post("/register")
-def register(request, user_data: UserRegistrationSchema):
-    if User.objects.filter(username=user_data.username).exists():
-        return {"success": False, "message": "Username already exists"}
-    
-    User.objects.create(
-        username=user_data.username,
-        password=make_password(user_data.password)
-    )
-    return {"success": True, "message": "Username registered succesfully"}
+@api_controller('/auth', tags=['Authentication'], permissions=[AllowAny])
+class AuthController:
+
+    @route.post('register', response={201: Dict, 400: Dict})
+    def register(self, user_data: UserRegistrationSchema) -> Dict:
+        if User.objects.filter(username=user_data.username).exists():
+            return 400, {"success": False, "message": "Username already exists"}
+        
+        User.objects.create_user(
+            username=user_data.username,
+            password=user_data.password
+        )
+        return 201, {"success": True, "message": "Registration successful"}
 
 @api_controller
 class JobController:
@@ -42,4 +45,4 @@ class JobController:
     
     
     
-api.register_controllers(NinjaJWTDefaultController, JobController)
+api.register_controllers(NinjaJWTDefaultController, AuthController, JobController)
