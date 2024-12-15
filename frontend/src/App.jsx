2510d13ca2715bec5db1d3c69ae6591e69892612
Pirat102 <patryk.pirowski@gmail.com>
2024-12-15 15:20:57 +0100

    prepare frontend functionality for applications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import JobsPage from "./pages/JobsPage";
+import Applications from "./pages/Applications";
 import ProtectedRoute from "./components/ProtectedRoute";
 import './styles/Base.css';
 import { LanguageProvider } from './contexts/LanguageContext';
@@ -35,6 +36,13 @@ function App() {
             <Route path="/register" element={<RegisterUser />} />
             <Route path="*" element={<NotFound />} />
             <Route path="/jobs" element={<JobsPage />} />
+            
+              <Route path="/applications" element={
+                <ProtectedRoute>
+                <Applications />
+                </ProtectedRoute> 
+              } />
+            
           </Routes>
         </div>
       </BrowserRouter>
