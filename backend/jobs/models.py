be7d139ff67a505e97d9c7ebd236a21aa95e4b5c
Pirat102 <patryk.pirowski@gmail.com>
2025-01-06 02:57:57 +0100

    auto_now overwriting created_at date

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -16,7 +16,7 @@ class Job(models.Model):
     scraped_date = models.DateTimeField(auto_now=timezone.now)
     summary = models.TextField(null=True, blank=True)
     source = models.CharField(max_length=20, null=True)
-    created_at = models.DateTimeField(auto_now=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"ID: {self.id} - Title: {self.title}"
@@ -91,7 +91,7 @@ class JobApplication(models.Model):
 class ApplicationNote(models.Model):
     application = models.ForeignKey(JobApplication, on_delete=models.CASCADE, related_name='notes')
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=timezone.now)
 
     class Meta:
