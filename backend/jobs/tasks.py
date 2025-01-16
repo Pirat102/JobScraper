6e748e4221bc06f78a2374fa04d347e820361157
Pirat102 <patryk.pirowski@gmail.com>
2025-01-16 08:49:02 +0100

    celery task, back to prod settings

--- backend/jobs/tasks.py
+++ backend/jobs/tasks.py
@@ -5,15 +5,17 @@ from django.core.cache import cache
 
 @shared_task
 def run_scrapers_task():
+    result = call_command("run_scrapers")
     
     ## Clear cache
+    time.sleep(300)
     redis_client = cache.client.get_client()
     cache_keys = redis_client.keys('*views.decorators.cache*')
     
     if cache_keys:
         redis_client.delete(*cache_keys)
             
-    return "result"
+    return result
     
     
         
